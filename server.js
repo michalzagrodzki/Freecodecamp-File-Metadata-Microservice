38e069d8b1ca9242efca9c29fed81068937feec1
Michal Zagrodzki <michalzagrodzki@gmail.com>
2021-08-03 14:05:35 +0200

    refactor: Change structure of server.js to be consistent with previous projects

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-var express = require("express");
-var cors = require("cors");
 require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
 
-var app = express();
+const bodyParser = require("body-parser");
+
+// Basic Configuration
+const { DEFAULT_PORT } = require("./utils/constants");
 
 // Controllers
 const file_controller = require("./domains/file/file.controller");
 
 app.use(cors());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // info page
 app.get("/", function (req, res) {
@@ -18,7 +27,6 @@ app.get("/", function (req, res) {
 // file endpoints
 app.post("/api/fileanalyse", file_controller.upload);
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log("Your app is listening on port " + port);
+const listener = app.listen(DEFAULT_PORT || process.env.PORT, function () {
+  console.log(`Listening on port ${listener.address().port}`);
 });
